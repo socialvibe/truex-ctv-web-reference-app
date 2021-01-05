5b5e26ef059bbcc220f63567bba173e1d7483a26
Michael Thomson <michaelt@ensemble.com>
2021-01-05 11:56:03 -0500

    removing all the socket server listener

--- platforms/XboxOne/RefApp/MainPage.xaml.cs
+++ platforms/XboxOne/RefApp/MainPage.xaml.cs
@@ -29,63 +29,21 @@ namespace RefApp
             this.StartServer();
         }
 
-        // See : https://docs.microsoft.com/en-us/windows/uwp/networking/sockets
-        private async void StartServer()
+        private void StartServer()
         {
-            try
-            {
-                var streamSocketListener = new Windows.Networking.Sockets.StreamSocketListener();
+            // The URI string
+            // var uriToLaunch = "http://192.168.2.65:8080/";
+            var uriToLaunch = "https://ctv.truex.com/web/ref-app/master/index.html";
 
-                // The ConnectionReceived event is raised when connections are received.
-                streamSocketListener.ConnectionReceived += this.StreamSocketListener_ConnectionReceived;
+            // Create a Uri object from the URI string
+            var uri = new Uri(uriToLaunch);
 
-                // Start listening for incoming TCP connections on the specified port. You can specify any port that's not currently in use.
-                await streamSocketListener.BindServiceNameAsync("8086");
-
-                Debug.WriteLine("server is listening...");
-
-                // The URI string
-                // var uriToLaunch = "http://192.168.2.65:8080/";
-                var uriToLaunch = "https://ctv.truex.com/web/ref-app/master/index.html";
-
-                // Create a Uri object from the URI string
-                var uri = new Uri(uriToLaunch);
-
-                MainWebView.Navigate(uri);
-            }
-            catch (Exception ex)
-            {
-                Windows.Networking.Sockets.SocketErrorStatus webErrorStatus = Windows.Networking.Sockets.SocketError.GetStatus(ex.GetBaseException().HResult);
-                Debug.WriteLine(webErrorStatus.ToString() != "Unknown" ? webErrorStatus.ToString() : ex.Message);
-            }
-        }
-        private async void StreamSocketListener_ConnectionReceived(Windows.Networking.Sockets.StreamSocketListener sender, Windows.Networking.Sockets.StreamSocketListenerConnectionReceivedEventArgs args)
-        {
-            string request;
-            using (var streamReader = new StreamReader(args.Socket.InputStream.AsStreamForRead()))
-            {
-                request = await streamReader.ReadLineAsync();
-            }
-
-            Uri newUrl = new Uri(request);
-
-            // Echo the request back the Url (string) as the response.
-            using (Stream outputStream = args.Socket.OutputStream.AsStreamForWrite())
-            {
-                using (var streamWriter = new StreamWriter(outputStream))
-                {
-                    await streamWriter.WriteLineAsync(newUrl.ToString());
-                    await streamWriter.FlushAsync();
-                }
-            }
-
-            // Reload Webpage
-            await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => MainWebView.Source = newUrl);
+            MainWebView.Navigate(uri);
         }
 
         private void MainWebView_DOMContentLoaded(WebView sender, WebViewDOMContentLoadedEventArgs args)
         {
-            // Skyline should be mapping xbox controller to keyboard.
+            // Ref App should be mapping xbox controller to keyboard.
             // _ = MainWebView.InvokeScriptAsync("eval", new string[] { "navigator.gamepadInputEmulation = 'keyboard';" });
         }
     }
